f74f931e3bfa6aecae6963223b5bf343ee82edd0
dinhtunglam <dinhtunglam1342005@gmail.com>
2026-06-16 02:42:37 +0700

    Xoa thu muc primary-school-management-g2

--- primary-school-management-g2/src/components/ImageCard.jsx
+++
@@ -1,9 +0,0 @@
-function ImageCard({ src, alt, className = '' }) {
-  return (
-    <div className={`image-frame ${className}`}>
-      <img src={src} alt={alt} onError={(event) => { event.currentTarget.style.display = 'none' }} />
-    </div>
-  )
-}
-
-export default ImageCard
